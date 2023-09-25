441a857f5918e5b153e57569a41363e0f05ce820
Arashii <51271100+ArashiSol@users.noreply.github.com>
2023-09-26 02:59:09 +0700

    Update index.tsx

--- src/games/HiLo/index.tsx
+++ src/games/HiLo/index.tsx
@@ -1,12 +1,12 @@
 import App from './Hilo'
-import image from './hilo1.png'
+import image from './hilo3.png'
 
 const game = {
   name: 'HiLo',
   short_name: 'hilo',
   description: 'Guess if the next card is going to be higher or lower than the current one. Continue until you want to cash out!',
   image,
-  theme_color: '#81c2ff',
+  theme_color: '#FAEBD0',
   app: App,
 }
 
