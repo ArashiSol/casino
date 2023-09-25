68c87d5f5096ce301cda944752e5748d6f3b4288
Arashii <51271100+ArashiSol@users.noreply.github.com>
2023-09-26 03:02:59 +0700

    Update index.tsx

--- src/games/Mines/index.tsx
+++ src/games/Mines/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import image from './mines.png'
+import image from './mines3.png'
 
 export default {
   name: 'Mines',
@@ -7,7 +7,7 @@ export default {
   description: `
     There's money hidden beneath the squares. The reward will increase the more squares you reveal, but watch out for the 5 hidden mines. Touch one and you'll go broke. You can cash out at any time.
   `,
-  theme_color: 'rgb(138 102 255)',
+  theme_color: '#EEE3EC',
   image,
   app: React.lazy(() => import('./Mines')),
 }
